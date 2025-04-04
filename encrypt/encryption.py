9124391ecb979244ec55c6de05f461f66bd7c673
Sherrybmd <shahryar.babamohamadi.2006@gmail.com>
2025-04-04 15:01:22 +0330

    Move to directory

---
+++ encrypt/encryption.py
@@ -0,0 +1,23 @@
+
+'''
+1_ start
+2_ take plain text and store
+3_ give txt to encode function
+4_ store encoded txt and show
+5_end
+
+1_start
+2_ take encoded text and store
+3_ give stored encoded txt to decode function
+4_ store decoded plain text and show
+5_ end
+'''
+
+
+def encode():
+    pass
+
+
+def decode():
+
+    pass

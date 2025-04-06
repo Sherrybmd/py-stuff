98cbe1b3dd563abb70d1b67c1906f0c5fa273d9c
Sherrybmd <shahryar.babamohamadi.2006@gmail.com>
2025-04-06 16:15:04 +0330

    Apply Untested solution template

--- fundamentals/exercises/plus-minus/plus-minus.py
+++ fundamentals/exercises/plus-minus/plus-minus.py
@@ -1,16 +1,27 @@
 def plusMinus(arr):
 
     pos = neg = zer = 0
+    max = len(arr)
 
     for num in arr:
-        if num > 1:
-            pass
+        if num > 0:
+            pos += 1
 
-        if num == 0:
-            pass
+        elif num == 0:
+            zer += 1
 
-        if num < 0:
-            pass
+        elif num < 0:
+            neg += 1
+
+        else:
+            error = "ERROR, cant read the array, enter numbers such as 1, 0, -1"
+            print(error)
+
+    posratio = pos/max
+    negratio = neg/max
+    zerratio = zer/max
+
+    print(posratio, negratio, zerratio)
 
 arr = [1,1,0,-1,-1]
 

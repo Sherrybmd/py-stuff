9cd80e49ad9a6ec14ec48cbad8ec86986f39b58f
Sherrybmd <shahryar.babamohamadi.2006@gmail.com>
2025-04-06 15:52:44 +0330

    issue fixed i repeat ISSUE FIXED NO IDEA HOW
    tweaked some shit and it worked, gotta take a look later

--- fundamentals/exercises/diagonal-sum/diagonal-sum.py
+++ fundamentals/exercises/diagonal-sum/diagonal-sum.py
@@ -9,7 +9,7 @@ def diag(arr):
             column = 0
         else:
             direction = -1
-            row = arr.index(arr[-1])
+            row = len(arr)-1
             column = 0
 
         while row < len(arr) and column < len(arr) and column >= 0 and row >= 0:
@@ -21,11 +21,11 @@ def diag(arr):
 
         return sum
     res = start(1) - start(-1)
-    print(abs(res))
     return abs(start(1) - start(-1))
 
+arr3 = [[1,1,1],[1,1,1],[1,1,1]]
 arr2 = [[1,2,3],[4,5,6,],[7,8,9]]
 arr = [[11,2,4],[4,5,6],[10,8,-12]]
-diag(arr)
+print(diag(arr3))
 
 

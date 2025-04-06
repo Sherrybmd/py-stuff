0f3d492962cbfe6c2eb95aa039197c352a90a31c
Sherrybmd <shahryar.babamohamadi.2006@gmail.com>
2025-04-06 16:07:22 +0330

    Start template, plus-minus proj

---
+++ fundamentals/exercises/plus-minus/plus-minus.py
@@ -0,0 +1,18 @@
+def plusMinus(arr):
+
+    pos = neg = zer = 0
+
+    for num in arr:
+        if num > 1:
+            pass
+
+        if num == 0:
+            pass
+
+        if num < 0:
+            pass
+
+arr = [1,1,0,-1,-1]
+
+
+plusMinus(arr)
